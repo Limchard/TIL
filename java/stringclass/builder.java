a8c806c5c84a90d049c89ece34fa646225454e9d
limchard <jksy6458@naver.com>
2024-01-19 23:09:32 +0900

    StringBuilder

---
+++ java/stringclass/builder.java
@@ -0,0 +1,34 @@
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+public class builder {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+
+		String data="자바";
+
+		// String -> byte 배열 (기본 : UTF-8 인코딩)
+		byte[] arr1=data.getBytes(); // byte[] arr1=data.getBytes(arr1)):
+		System.out.println("arr1: "+ Arrays.toString(arr1));
+
+		// byte 배열 -> String (기본 : UTF-8 디코딩)
+		String str1=new String(arr1); // String str1=new String(arr1, "UTF-8");
+		System.out.println("str1: "+str1);
+
+		// String -> byte 배열(EUC-KR 인코딩)
+		byte[] arr2=data.getBytes("EUC-KR");
+		System.out.println("arr2: "+Arrays.toString(arr2));
+
+		// byte 배열 -> String(기본: UTF-8 디코딩)
+		String str2=new String(arr2,"EUC-KR");
+		System.out.println("str2: "+str2);
+
+
+		String sb=new StringBuilder()
+			.append("DEF")
+			.insert(0,"ABC")
+			.delete(3,4)
+			.toString();
+		System.out.println(sb);
+
+	}
+}
---
+++ java/stringclass/out/production/stringclass/.idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+<
+builder.java,d/1/d134c3e2d455e4320e90ef67f0e5d85415986bad
