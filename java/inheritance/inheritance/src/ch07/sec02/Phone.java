04a89a568e723ba9d264aab1f15342a522f61d0b
lhj <jksy6458@naver.com>
2024-06-11 10:43:08 +0900

    이것이 자바다 상속  7.3부모생성자 호출'

--- java/inheritance/inheritance/src/ch07/sec02/Phone.java
+++ java/inheritance/inheritance/src/ch07/sec02/Phone.java
@@ -7,18 +7,23 @@ public class Phone
     public String color;
 
     // 메소드 선언
-    public void bell(){
+    public void bell()
+    {
         System.out.println("벨이 울립니다.");
     }
-    public void sendVoice(String message){
-        System.out.println("자기: "+message);
+
+    public void sendVoice(String message)
+    {
+        System.out.println("자기: " + message);
     }
 
     public void receiveVoice(String message)
     {
-        System.out.println("상대방: "+message);
+        System.out.println("상대방: " + message);
     }
-    public void hangUp(){
+
+    public void hangUp()
+    {
         System.out.println("전화를 끊습니다.");
     }
 }
